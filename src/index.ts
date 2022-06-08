1da586fa8d60c46cb05b9c5625d8e7c927a23491
Vitor Daniel <vitor036daniel@gmail.com>
2022-06-08 10:00:06 -0300

    feat: ✨ add more move and draw functions

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import { G, Polygon, SVG } from "@svgdotjs/svg.js"
 export class Turtle {
   angle: number = 0
   position: [number, number] = [0, 0]
-  group: G
+  private group: G
   turtle: Polygon
   isPenDown: boolean = true
-  draw = SVG().addTo("#turtle")
+  private draw = SVG().addTo("#turtle").viewbox("-256 -256 512 512")
+  private stamps = []
 
   constructor() {
     this.group = this.draw.group()
@@ -97,5 +98,52 @@ export class Turtle {
 
   async sety(y: number) {
     await this.move(this.position[0], y)
+  }
+
+  async setheading(angle: number) {
+    await this.right(angle - this.angle)
+  }
+
+  seth = this.setheading
+
+  async home() {
+    await this.goto(0, 0)
+  }
+
+  async circle(
+    radius: number,
+    extent: number = 360,
+    steps: number | null = null
+  ) {
+    if (steps === null) {
+      steps = Math.ceil(Math.abs(extent) / 10)
+    }
+    const angle = extent / steps
+    for (let i = 0; i < steps; i++) {
+      await this.forward(radius * Math.sin((angle * Math.PI) / 180))
+      await this.right(angle)
+    }
+  }
+
+  dot(size: number = 1, color: string = "black") {
+    this.draw
+      .circle(size)
+      .fill(color)
+      .center(this.position[0] + 15, this.position[1] + 7.5)
+  }
+
+  stamp() {
+    this.stamps.push(
+      this.draw
+        .polygon("0,0 4,7.5 0,15 15,7.5")
+        .center(this.position[0], this.position[1])
+    )
+
+    return this.stamps.length - 1
+  }
+
+  clearstemp(id: number) {
+    this.stamps[id].remove()
+    this.stamps.splice(id, 1)
   }
 }
