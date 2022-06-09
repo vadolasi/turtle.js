481e43c4cae5809dfe7a347dfd01759c61b40de9
Vitor Daniel <vitor036daniel@gmail.com>
2022-06-09 08:48:07 -0300

    feat: ✨ add state functions

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,32 @@
-import { G, Polygon, SVG } from "@svgdotjs/svg.js"
+import { Polygon, SVG } from "@svgdotjs/svg.js"
 
 export class Turtle {
-  angle: number = 0
-  position: [number, number] = [0, 0]
-  private group: G
-  turtle: Polygon
-  isPenDown: boolean = true
+  private angle: number = 0
+  private _position: [number, number] = [0, 0]
+  private isPenDown: boolean = true
   private draw = SVG().addTo("#turtle").viewbox("-256 -256 512 512")
+  private group = this.draw.group()
+  private turtle = this.group.polygon("0,0 4,7.5 0,15 15,7.5").center(0, 0)
   private stamps: { [key: number]: Polygon } = {}
   private nextStampId: number = 1
   private speedNormalize = 3
   private _speed = 6
 
-  constructor() {
-    this.group = this.draw.group()
-    this.turtle = this.group.polygon("0,0 4,7.5 0,15 15,7.5").center(0, 0)
-  }
-
   private drawLine(x: number, y: number) {
     const distance = Math.sqrt(
-      Math.pow(x - this.position[0], 2) + Math.pow(y - this.position[1], 2)
+      Math.pow(x - this._position[0], 2) + Math.pow(y - this._position[1], 2)
     )
 
     this.draw
       .line(
-        this.position[0],
-        this.position[1],
-        this.position[0],
-        this.position[1]
+        this._position[0],
+        this._position[1],
+        this._position[0],
+        this._position[1]
       )
       .stroke({ width: 1, color: "black" })
       .animate((distance / this._speed) * this.speedNormalize)
-      .plot(this.position[0], this.position[1], x, y)
+      .plot(this._position[0], this._position[1], x, y)
   }
 
   private async move(x: number, y: number) {
@@ -41,7 +36,7 @@ export class Turtle {
 
     await new Promise((resolve) => {
       const distance = Math.sqrt(
-        Math.pow(x - this.position[0], 2) + Math.pow(y - this.position[1], 2)
+        Math.pow(x - this._position[0], 2) + Math.pow(y - this._position[1], 2)
       )
 
       this.group
@@ -49,15 +44,15 @@ export class Turtle {
         .center(x, y)
         .after(resolve)
     })
-    this.position[0] = x
-    this.position[1] = y
+    this._position[0] = x
+    this._position[1] = y
   }
 
   async forward(distance: number) {
     const xMove = distance * Math.cos((this.angle * Math.PI) / 180)
     const yMove = distance * Math.sin((this.angle * Math.PI) / 180)
 
-    await this.move(this.position[0] + xMove, this.position[1] - yMove)
+    await this.move(this._position[0] + xMove, this._position[1] - yMove)
   }
 
   fd = this.forward
@@ -107,14 +102,14 @@ export class Turtle {
   }
 
   setpos = this.goto
-  setposition = this.goto
+  set_position = this.goto
 
   async setx(x: number) {
-    await this.move(x, this.position[1])
+    await this.move(x, this._position[1])
   }
 
   async sety(y: number) {
-    await this.move(this.position[0], y)
+    await this.move(this._position[0], y)
   }
 
   async setheading(angle: number) {
@@ -127,14 +122,10 @@ export class Turtle {
     await this.goto(0, 0)
   }
 
-  async circle(
-    radius: number,
-    extent: number = 360,
-    steps: number | null = null
-  ) {
+  async circle(radius: number, extent: number = 360, steps?: number) {
     const circlePrecision = 10
 
-    if (steps === null) {
+    if (steps) {
       steps = Math.ceil(Math.abs(extent) / circlePrecision)
     }
     const angle = extent / steps
@@ -148,14 +139,14 @@ export class Turtle {
     this.draw
       .circle(size)
       .fill(color)
-      .center(this.position[0] + 15, this.position[1] + 7.5)
+      .center(this._position[0] + 15, this._position[1] + 7.5)
   }
 
   stamp() {
     const id = this.nextStampId
     const polygon = this.draw
       .polygon("0,0 4,7.5 0,15 15,7.5")
-      .center(this.position[0], this.position[1])
+      .center(this._position[0], this._position[1])
       .rotate(-this.angle)
 
     this.stamps[id] = polygon
@@ -170,8 +161,8 @@ export class Turtle {
     delete this.stamps[id]
   }
 
-  clearstamps(quantity: number | null = null) {
-    if (quantity === null) {
+  clearstamps(quantity?: number) {
+    if (quantity) {
       quantity = Object.keys(this.stamps).length
     }
 
@@ -199,15 +190,61 @@ export class Turtle {
   }
 
   // TODO: implement this function
-  async undo(quantity: number | null = null) {}
+  async undo(quantity?: number) {}
 
   speed(): number
   speed(speed: number): void
-  speed(speed: number | null = null) {
-    if (speed === null) {
+  speed(speed?: number) {
+    if (speed) {
       return this._speed
     } else {
       this._speed = speed
     }
+  }
+
+  position() {
+    return this._position
+  }
+
+  pos = this.position
+
+  towards(x: number, y: number) {
+    const angle = Math.atan2(y - this._position[1], x - this._position[0])
+    return (angle * 180) / Math.PI
+  }
+
+  xcor() {
+    return this._position[0]
+  }
+
+  ycor() {
+    return this._position[1]
+  }
+
+  heading() {
+    return this.angle
+  }
+
+  distance(x: number, y: number): number
+  distance(cords: [number, number]): number
+  distance(fromTurtle: Turtle): number
+  distance(arg1: number | [number, number] | Turtle, y?: number): number {
+    let fromX: number
+    let fromY: number
+
+    if (typeof arg1 === "number") {
+      fromX = arg1
+      fromY = y
+    } else if (arg1 instanceof Turtle) {
+      ;[fromX, fromY] = arg1.position()
+    } else {
+      fromX = arg1[0]
+      fromY = arg1[1]
+    }
+
+    return Math.sqrt(
+      Math.pow(fromX - this._position[0], 2) +
+        Math.pow(fromY - this._position[1], 2)
+    )
   }
 }
